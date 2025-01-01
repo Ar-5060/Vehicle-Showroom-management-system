603e1ffc05b6daa546f48c65c098181fd36a03d7
Anisur Rahman <168066714+Ar-5060@users.noreply.github.com>
2025-01-01 20:43:04 +0600

    Update LoginSystem.java

--- Project/LoginSystem.java
+++ Project/LoginSystem.java
@@ -3,13 +3,13 @@ package Projects;
 class LoginSystem {
     private User user;
 
-    // Register a new user
+    
     public void registerUser(String username, String password) {
         user = new User(username, password);
         System.out.println("User registered successfully!");
     }
 
-    // Authenticate login
+    
     public boolean login(String username, String password) {
         if (user != null && user.getUsername().equals(username) && user.getPassword().equals(password)) {
             return true;
@@ -17,7 +17,7 @@ class LoginSystem {
         return false;
     }
 
-    // Check if a user is already registered
+   
     public boolean isUserRegistered() {
         return user != null;
     }
